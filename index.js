7217c5e0c71900cf10b690edf78ed0dab70858f5
Raúl Rafael Carvajal Gutiérrez <Ral Rafael Carvajal Gutirrez>
2019-02-26 10:04:17 -0700

    Add changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ fs.readFile('list.csv','utf8',(err, data) => {
     line.shift();
     line.forEach((reg)=>{
         var aux=reg.split(",")
-        textToSave=textToSave.concat(`#${aux[0]}\nNúmero de control: ${aux[1]}\nNombre: ${aux[2]}\nCalificación: ${aux[3]}\n-----------------------------------------`);
+        textToSave=textToSave.concat(`#${aux[0]}\nNúmero de control: ${aux[1]}\nNombre: ${aux[2]}\nCalificación: ${aux[3]}\n-----------------------------------------\n`);
     })
     save(textToSave);
 });
